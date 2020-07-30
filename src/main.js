62e15d939065f361c7e3f69fa9d45d1219d341c2
SldTir <54772323+SldTir@users.noreply.github.com>
2020-07-30 16:19:18 +0300

    Исправил замечания линтера

--- src/main.js
+++ src/main.js
@@ -7,27 +7,27 @@ const createsUserMenu = () => {
     `<section class="header__profile profile">
       <p class="profile__rating">Movie Buff</p>
       <img class="profile__avatar" src="images/bitmap@2x.png" alt="Avatar" width="35" height="35">
-    </section>`  
-  );    
+    </section>`
+  );  
 };
 
 const createSiteNavigation = () => {
   return (
-   `<nav class="main-navigation">
+  `<nav class="main-navigation">
      <div class="main-navigation__items">
        <a href="#all" class="main-navigation__item main-navigation__item--active">All movies</a>
        <a href="#watchlist" class="main-navigation__item">Watchlist <span class="main-navigation__item-count">13</span></a>
        <a href="#history" class="main-navigation__item">History <span class="main-navigation__item-count">4</span></a>
        <a href="#favorites" class="main-navigation__item">Favorites <span class="main-navigation__item-count">8</span></a>
      </div>
-     <a href="#stats" class="main-navigation__additional">Stats</a> 
+     <a href="#stats" class="main-navigation__additional">Stats</a>
    </nav>`   
   );
 };
 
 const createSiteSort = () => {
   return(
-    `<ul class="sort">
+  `<ul class="sort">
       <li><a href="#" class="sort__button sort__button--active">Sort by default</a></li>
       <li><a href="#" class="sort__button">Sort by date</a></li>
       <li><a href="#" class="sort__button">Sort by rating</a></li>
@@ -37,19 +37,19 @@ const createSiteSort = () => {
 
 const createSiteFilmsContainer = () => {
   return(
-    `<section class="films">
-      <section class="films-list">
-        <h2 class="films-list__title visually-hidden">All movies. Upcoming</h2>
-  
-        <div class="films-list__container">
-        </div>        
-      </section>`  
+  `<section class="films">
+    <section class="films-list">
+      <h2 class="films-list__title visually-hidden">All movies. Upcoming</h2>  
+      <div class="films-list__container">
+      </div>        
+    </section>
+  </section>`
   );
 };
 
 const createSiteFilm = () => {
   return(
-    `<article class="film-card">
+  `<article class="film-card">
     <h3 class="film-card__title">The Dance of Life</h3>
     <p class="film-card__rating">8.3</p>
     <p class="film-card__info">
@@ -65,13 +65,13 @@ const createSiteFilm = () => {
       <button class="film-card__controls-item button film-card__controls-item--mark-as-watched">Mark as watched</button>
       <button class="film-card__controls-item button film-card__controls-item--favorite">Mark as favorite</button>
     </form>
-    </article>`  
+  </article>`
   );
 };
 
 const createSitePopup = () => {
   return(
-    `<section class="film-details">
+  `<section class="film-details">
     <form class="film-details__inner" action="" method="get">
       <div class="form-details__top-container">
         <div class="film-details__close">
@@ -245,7 +245,7 @@ const createSitePopup = () => {
 
 const createSiteShowMoreButton = () => {
   return(
-    `<button class="films-list__show-more">Show more</button>`
+  `<button class="films-list__show-more">Show more</button>`
   );
 };
   
