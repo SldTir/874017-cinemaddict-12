c7e75ad0448048d2f89f5cc23e733b466470b186
SldTir <54772323+SldTir@users.noreply.github.com>
2020-08-07 15:19:02 +0300

    Настроил логику допказа карточек и удаления кнопки допаказа

--- src/main.js
+++ src/main.js
@@ -8,6 +8,7 @@ import {createSiteShowMoreButton} from "./view/show-more-button.js";
 import {generateFilm} from "./mock/film.js";
 
 const FILM_COUNT = 15;
+const FILM_COUNT_PER_STEP = 5;
 
 const films = new Array(FILM_COUNT).fill().map(generateFilm);
 
@@ -25,12 +26,31 @@ render(siteMainElement, createSiteSort(), `beforeend`);
 render(siteMainElement, createSiteFilmsContainer(), `beforeend`);
 const siteFilmsContainer = siteMainElement.querySelector(`.films-list__container`);
 
-for (let i = 0; i < FILM_COUNT; i++) {
+for (let i = 0; i < Math.min(films.length, FILM_COUNT_PER_STEP); i++) {
   render(siteFilmsContainer, createSiteFilm(films[i]), `beforeend`);
 }
 
-const siteFilmElement = siteMainElement.querySelector(`.films`);
-render(siteFilmElement, createSiteShowMoreButton(), `beforeend`);
-
 const siteFooterElement = document.querySelector(`.footer`);
 render(siteFooterElement, createSitePopup(films[0]), `afterEnd`);
+
+if (films.length > FILM_COUNT_PER_STEP) {
+  let renderedFilmCount = FILM_COUNT_PER_STEP;
+
+  const siteFilmElement = siteMainElement.querySelector(`.films`);
+  render(siteFilmElement, createSiteShowMoreButton(), `beforeend`);
+
+  const showMoreButton = siteMainElement.querySelector(`.films-list__show-more`);
+  showMoreButton.addEventListener(`click`, (evt) => {
+    evt.preventDefault();
+
+    films
+      .slice(renderedFilmCount, renderedFilmCount + FILM_COUNT_PER_STEP)
+      .forEach((film) => render(siteFilmsContainer, createSiteFilm(film), `beforeend`));
+
+    renderedFilmCount += FILM_COUNT_PER_STEP;
+
+    if (renderedFilmCount >= films.length) {
+      showMoreButton.remove();
+    }
+  });
+}
