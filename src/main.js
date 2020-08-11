e0ee9671ac7aae90f538313eb62e2804869de861
SldTir <54772323+SldTir@users.noreply.github.com>
2020-08-11 11:15:22 +0300

    Рефакторинг рендер-метода

--- src/main.js
+++ src/main.js
@@ -6,38 +6,35 @@ import {createSiteFilm} from "./view/film.js";
 import {createSitePopup} from "./view/popup.js";
 import {createSiteShowMoreButton} from "./view/show-more-button.js";
 import {generateFilm} from "./mock/film.js";
+import {renderTemplate} from "./utils.js";
 
 const FILM_COUNT = 15;
 const FILM_COUNT_PER_STEP = 5;
 
 const films = new Array(FILM_COUNT).fill().map(generateFilm);
 
-const render = (container, template, place) => {
-  container.insertAdjacentHTML(place, template);
-};
-
 const siteHeaderElement = document.querySelector(`.header`);
 const siteMainElement = document.querySelector(`.main`);
 
-render(siteHeaderElement, createsUserMenu(), `beforeend`);
-render(siteMainElement, createSiteNavigation(films), `beforeend`);
-render(siteMainElement, createSiteSort(), `beforeend`);
+renderTemplate(siteHeaderElement, createsUserMenu(), `beforeend`);
+renderTemplate(siteMainElement, createSiteNavigation(films), `beforeend`);
+renderTemplate(siteMainElement, createSiteSort(), `beforeend`);
 
-render(siteMainElement, createSiteFilmsContainer(), `beforeend`);
+renderTemplate(siteMainElement, createSiteFilmsContainer(), `beforeend`);
 const siteFilmsContainer = siteMainElement.querySelector(`.films-list__container`);
 
 for (let i = 0; i < Math.min(films.length, FILM_COUNT_PER_STEP); i++) {
-  render(siteFilmsContainer, createSiteFilm(films[i]), `beforeend`);
+  renderTemplate(siteFilmsContainer, createSiteFilm(films[i]), `beforeend`);
 }
 
 const siteFooterElement = document.querySelector(`.footer`);
-render(siteFooterElement, createSitePopup(films[0]), `afterEnd`);
+renderTemplate(siteFooterElement, createSitePopup(films[0]), `afterEnd`);
 
 if (films.length > FILM_COUNT_PER_STEP) {
   let renderedFilmCount = FILM_COUNT_PER_STEP;
 
   const siteFilmElement = siteMainElement.querySelector(`.films`);
-  render(siteFilmElement, createSiteShowMoreButton(), `beforeend`);
+  renderTemplate(siteFilmElement, createSiteShowMoreButton(), `beforeend`);
 
   const showMoreButton = siteMainElement.querySelector(`.films-list__show-more`);
   showMoreButton.addEventListener(`click`, (evt) => {
@@ -45,7 +42,7 @@ if (films.length > FILM_COUNT_PER_STEP) {
 
     films
       .slice(renderedFilmCount, renderedFilmCount + FILM_COUNT_PER_STEP)
-      .forEach((film) => render(siteFilmsContainer, createSiteFilm(film), `beforeend`));
+      .forEach((film) => renderTemplate(siteFilmsContainer, createSiteFilm(film), `beforeend`));
 
     renderedFilmCount += FILM_COUNT_PER_STEP;
 
