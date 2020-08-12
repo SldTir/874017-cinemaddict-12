b1c0f1420276c035b9836d0f9102ba8d517f493e
SldTir <54772323+SldTir@users.noreply.github.com>
2020-08-12 13:04:37 +0300

    Рефактор метода рендер

--- src/main.js
+++ src/main.js
@@ -6,9 +6,9 @@ import Film from "./view/film.js";
 import Popup from "./view/popup.js";
 import SchowMoreButtonView from "./view/show-more-button.js";
 import {generateFilm} from "./mock/film.js";
-import {renderElement, RenderPosition} from "./utils.js";
+import {render, RenderPosition} from "./utils.js";
 
-const FILM_COUNT = 15;
+const FILM_COUNT = 13;
 const FILM_COUNT_PER_STEP = 5;
 
 const films = new Array(FILM_COUNT).fill().map(generateFilm);
@@ -18,32 +18,32 @@ const siteMainElement = document.querySelector(`.main`);
 const siteFooterElement = document.querySelector(`.footer`);
 
 
-renderElement(siteHeaderElement, new UserMenuView().getElement(), RenderPosition.BEFOREEND);
-renderElement(siteMainElement, new SiteFilter(films).getElement(), RenderPosition.BEFOREEND);
-renderElement(siteMainElement, new SortView().getElement(), RenderPosition.BEFOREEND);
+render(siteHeaderElement, new UserMenuView().getElement(), RenderPosition.BEFOREEND);
+render(siteMainElement, new SiteFilter(films).getElement(), RenderPosition.BEFOREEND);
+render(siteMainElement, new SortView().getElement(), RenderPosition.BEFOREEND);
 
 const filmsContainerComponent = new FilmsContainerView();
-renderElement(siteMainElement, filmsContainerComponent.getElement(), RenderPosition.BEFOREEND);
+render(siteMainElement, filmsContainerComponent.getElement(), RenderPosition.BEFOREEND);
 
 const siteFilmsListContainer = siteMainElement.querySelector(`.films-list__container`);
 for (let i = 0; i < Math.min(films.length, FILM_COUNT_PER_STEP); i++) {
-  renderElement(siteFilmsListContainer, new Film(films[i]).getElement(), RenderPosition.BEFOREEND);
+  render(siteFilmsListContainer, new Film(films[i]).getElement(), RenderPosition.BEFOREEND);
 }
 
-renderElement(siteFooterElement, new Popup(films[0]).getElement(), RenderPosition.AFTERBEGIN);
+render(siteFooterElement, new Popup(films[0]).getElement(), RenderPosition.AFTERBEGIN);
 
 if (films.length > FILM_COUNT_PER_STEP) {
   const schowMoreButtonComponent = new SchowMoreButtonView();
   let renderedFilmCount = FILM_COUNT_PER_STEP;
 
-  renderElement(filmsContainerComponent.getElement(), schowMoreButtonComponent.getElement(), RenderPosition.BEFOREEND);
+  render(filmsContainerComponent.getElement(), schowMoreButtonComponent.getElement(), RenderPosition.BEFOREEND);
 
   schowMoreButtonComponent.getElement().addEventListener(`click`, (evt) => {
     evt.preventDefault();
 
     films
       .slice(renderedFilmCount, renderedFilmCount + FILM_COUNT_PER_STEP)
-      .forEach((film) => renderElement(siteFilmsListContainer, new Film(film).getElement(), RenderPosition.BEFOREEND));
+      .forEach((film) => render(siteFilmsListContainer, new Film(film).getElement(), RenderPosition.BEFOREEND));
 
     renderedFilmCount += FILM_COUNT_PER_STEP;
 
