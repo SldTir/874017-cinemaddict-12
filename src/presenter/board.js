305203ce6f8ca5d0038faa879844b6ea83c3bf8d
SldTir <54772323+SldTir@users.noreply.github.com>
2020-08-15 23:29:50 +0300

    Описал логику презентера доски

--- src/presenter/board.js
+++ src/presenter/board.js
@@ -6,15 +6,64 @@ import NoFilmView from "../view/no-film.js";
 import ShowMoreButtonView from "../view/show-more-button.js";
 import {render, RenderPosition} from "./utils/render.js";
 
-export default Board {
+
+const FILM_COUNT_PER_STEP = 5;
+
+export default class Board {
   constructor(boardContainer) {
     this._boardContainer = boardContainer;
-    
+
     this._boardComponent = new BoardView();
     this._sortComponent = new SortView();
     this._filmComponent = new FilmView();
     this._popupComponent = new PopupView();
     this._noFilmComponent = new NoFilmView();
     this._showMoreButtonComponent = new ShowMoreButtonView();
+  }
+
+  init(boardFilms) {
+    this._boardFilms = boardFilms.slice();
+
+    render(this._boardContainer, this._boardComponent, RenderPosition.BEFOREEND);
+   
+    this._renderBoard();
+  }
+
+  _renderSort() {
+
+  }
+
+  _renderFilm(from, to) {
+    
+  }
+
+  _renderNoFilm() {
+    
+  }
+
+  _renderShowMoreButton() {
+    
+  }
+
+  _renderBoard(boardContainer) {
+    const filmsContainerComponent = this._boardComponent;
+    render(boardContainer, filmsContainerComponent, RenderPosition.BEFOREEND);
+
+    const siteFilmsListContainer = boardContainer.querySelector(`.films-list__container`);
+    const siteFilmList = boardContainer.querySelector(`.films-list`);
+
+    if (this._boardFilms.length === 0) {
+      siteFilmList.innerHTML = ``;
+      render(siteFilmList, this._noFilmComponent, RenderPosition.BEFOREEND);
+      return;
+    }
+
+    this._boardFilms
+    .slice(0, Math.min(this._boardFilms.length, FILM_COUNT_PER_STEP))
+    .forEach((boardFilm) => this._renderFilm(siteFilmsListContainer, boardFilm));
+
+    if (this._boardTasks.length > FILM_COUNT_PER_STEP) {
+      this._renderShowMoreButton();
+    }
   }
 }
