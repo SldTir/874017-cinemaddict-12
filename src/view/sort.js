1e75204634cf8e85ef00943fa3b081ecccbbe87c
SldTir <54772323+SldTir@users.noreply.github.com>
2020-08-17 21:19:53 +0300

    Реализовал подписку на изменение сортировки

--- src/view/sort.js
+++ src/view/sort.js
@@ -12,7 +12,27 @@ const createSiteSort = () => {
 };
 
 export default class SiteSort extends AbstractView {
+  constructor() {
+    super();
+
+    this._sortTypeChangeHandler = this._sortTypeChangeHandler.bind(this);
+  }
+
   getTemplate() {
     return createSiteSort();
+  }
+
+  _sortTypeChangeHandler(evt) {
+    if (evt.target.tagName !== `A`) {
+      return;
+    }
+
+    evt.preventDefault();
+    this._callback.sortTypeChange(evt.target.dataset.sortType);
+  }
+
+  setSortTypeChangeHandler(callback) {
+    this._callback.sortTypeChange = callback;
+    this.getElement().addEventListener(`click`, this._sortTypeChangeHandler);
   }
 }
