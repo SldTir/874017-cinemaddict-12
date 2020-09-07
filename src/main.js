879397c257afb2761a813c327126aa9b8cd9a2a8
SldTir <54772323+SldTir@users.noreply.github.com>
2020-09-07 12:08:13 +0300

    Добавил презентер фильтров

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import UserMenuView from "./view/user-menu.js";
-import SiteFilter from "./view/filter.js";
 import MoveListPresenter from "./presenter/move-list.js";
 import FilmsModel from "./model/film.js";
 import CommentsModel from "./model/comments.js";
+import FilterPresenter from "./presenter/filter.js";
 import FilterModel from "./model/filter.js";
 import {generateFilm, generateComment} from "./mock/film.js";
 import {render, RenderPosition} from "./utils/render.js";
@@ -23,7 +23,8 @@ commentsModel.setComments(comments);
 const siteHeaderElement = document.querySelector(`.header`);
 const siteMainElement = document.querySelector(`.main`);
 const moveListPresenter = new MoveListPresenter(siteMainElement, filmsModel, commentsModel);
+const filterPresenter = new FilterPresenter(siteMainElement, filterModel, filmsModel);
 
 render(siteHeaderElement, new UserMenuView(), RenderPosition.BEFOREEND);
-render(siteMainElement, new SiteFilter(films), RenderPosition.BEFOREEND);
+filterPresenter.init();
 moveListPresenter.init();
--- src/view/filter.js
+++ src/view/filter.js
@@ -1,17 +1,27 @@
 import AbstractView from "./abstract.js";
+import {FilterType} from "../const";
 
-const createSiteFilter = (films) => {
-  const watchlist = films.filter((film) => film.watchlist === true).length;
-  const history = films.filter((film) => film.history === true).length;
-  const favorites = films.filter((film) => film.favorites === true).length;
+const createFilterItemTemplate = (filter, currentFilterType) => {
+  const {type, name, count} = filter;
+  debugger;
+  const activeFilter = currentFilterType === type ? `main-navigation__item--active` : ``;
+  let filterContent;
+  if (FilterType.ALL_MOVIES === type) {
+    filterContent = type;
+  } else {
+    filterContent = count > 5 ? type : `${type} <span class="main-navigation__item-count">${count}</span>`;
+  }
+  return (
+    `<a href="${name}" class="main-navigation__item ${activeFilter}">${filterContent}</a>`
+  );
+};
 
+const createSiteFilterTemplate = (filterItems, currentFilterType) => {
+  const filterItemsTemplate = filterItems.map((filter) => createFilterItemTemplate(filter, currentFilterType)).join(``);
   return (
     `<nav class="main-navigation">
      <div class="main-navigation__items">
-       <a href="#all" class="main-navigation__item main-navigation__item--active">All movies</a>
-       <a href="#watchlist" class="main-navigation__item">Watchlist <span class="main-navigation__item-count">${watchlist}</span></a>
-       <a href="#history" class="main-navigation__item">History <span class="main-navigation__item-count">${history}</span></a>
-       <a href="#favorites" class="main-navigation__item">Favorites <span class="main-navigation__item-count">${favorites}</span></a>
+       ${filterItemsTemplate}
      </div>
      <a href="#stats" class="main-navigation__additional">Stats</a>
    </nav>`
@@ -19,13 +29,26 @@ const createSiteFilter = (films) => {
 };
 
 export default class FilterView extends AbstractView {
-  constructor(films) {
+  constructor(filters, currentFilterType) {
     super();
 
-    this._films = films;
+    this._filters = filters;
+    this._currentFilter = currentFilterType;
+
+    this._filterTypeChangeHandler = this._filterTypeChangeHandler.bind(this);
   }
 
   getTemplate() {
-    return createSiteFilter(this._films);
+    return createSiteFilterTemplate(this._filters, this._currentFilter);
+  }
+
+  _filterTypeChangeHandler(evt) {
+    evt.preventDefatult();
+    this._callback.filterTypeChange(evt.target.value);
+  }
+
+  setFilterTypeChangeHandler(callback) {
+    this._callback.filterTypeChange = callback;
+    this.getElement().addEventListener(`change`, this._filterTypeChangeHandler);
   }
 }
