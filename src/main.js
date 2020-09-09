5de4438f1de520cee768c14ecae297e8acef646e
SldTir <54772323+SldTir@users.noreply.github.com>
2020-09-09 15:29:22 +0300

    Рефакториг инициализации приложения

--- src/main.js
+++ src/main.js
@@ -4,36 +4,35 @@ import FilmsModel from "./model/film.js";
 import CommentsModel from "./model/comments.js";
 import FilterPresenter from "./presenter/filter.js";
 import FilterModel from "./model/filter.js";
-import {generateFilm, generateComment} from "./mock/film.js";
+import {generateComment} from "./mock/film.js";
 import {render, RenderPosition} from "./utils/render.js";
 import Api from "./api.js";
+
+const siteHeaderElement = document.querySelector(`.header`);
+const siteMainElement = document.querySelector(`.main`);
 
 const FILM_COUNT = 15;
 const AUTHORIZATION = `Basic er883jdzbdw`;
 const END_POINT = `https://12.ecmascript.pages.academy/cinemaddict`;
 
 const api = new Api(END_POINT, AUTHORIZATION);
-
-const films = new Array(FILM_COUNT).fill().map((element, id) => generateFilm(id));
 const comments = new Array(FILM_COUNT).fill().map((element, id) => generateComment(id));
-
-api.getFilms().then((films) => {
-  console.log(films);
-});
 
 const filmsModel = new FilmsModel();
 const commentsModel = new CommentsModel();
 
 const filterModel = new FilterModel();
 
-filmsModel.setFilms(films);
 commentsModel.setComments(comments);
 
-const siteHeaderElement = document.querySelector(`.header`);
-const siteMainElement = document.querySelector(`.main`);
+
 const moveListPresenter = new MoveListPresenter(siteMainElement, filmsModel, commentsModel, filterModel);
 const filterPresenter = new FilterPresenter(siteMainElement, filterModel, filmsModel);
 
 render(siteHeaderElement, new UserMenuView(), RenderPosition.BEFOREEND);
 filterPresenter.init();
 moveListPresenter.init();
+
+api.getFilms().then((films) => {
+  filmsModel.setFilms(films);
+});
